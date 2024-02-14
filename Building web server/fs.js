f123577d122edc851225a54fcea75d581d592507
Yeam <yeamlakneheshetu9@gmail.com>
2024-02-14 10:09:44 +0300

    event practice

--- Building web server/fs.js
+++ Building web server/fs.js
@@ -1,1 +1,12 @@
-console.log('Hello')
+// console.log('Hello')
+
+let events = require('events');
+let eventEmmiter = new events.EventEmitter();
+
+let myEvent = function () {
+    console.log("Yes, I am here!");
+}
+
+eventEmmiter.on("John", myEvent);
+
+eventEmmiter.emit("John");
